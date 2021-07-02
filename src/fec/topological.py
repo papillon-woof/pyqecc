15076353673499c415522c04b5746129a4c2b6fd
hy4 <ogyogugigegmail.com>
2021-07-02 23:06:35 +0900

    commit

---
+++ src/fec/topological.py
@@ -0,0 +1,26 @@
+import numpy as np
+from .abstruct import *
+from ..util import *
+
+def TORIC(SC):
+    def __init__(self,n,k,G='random',T=None,L=None,P=None,iid=True):
+        #H is replesentation for graph into adjacent matrix:
+        self._G = G
+        self._H = g2c(H)
+        self._name = "toric code"
+        self._n = n
+        self._k = k
+        self._R = self._k/self._n
+        if H in ['random']:
+            pass
+        else:
+            self._H = H
+        self._T = T
+        self._L = L
+
+        self.enc_circuit = None
+        self.dec_circuit = None
+        self._P = self.set_P(P)
+        self.ML_decoding_qubit_limit = 15
+
+    def decoder(self,H):
---
+++ src/util/util_topological.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+def g2c(H):
+    '''
+    This function adjacent matrix convert to parity matrix or stabilizer code.
+    '''
+
+    
