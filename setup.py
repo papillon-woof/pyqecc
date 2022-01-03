b4cf216d736b0f8a2a4c5f309ae0f477d71c8ac7
papillon-woof <ogyogugige@gmail.com>
2022-01-03 22:09:39 +0900

    commit

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# long_description(後述)に、GitHub用のREADME.mdを指定
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='pyqec', # パッケージ名(プロジェクト名)
+    packages=['pyqec'], # パッケージ内(プロジェクト内)のパッケージ名をリスト形式で指定
+    version='0.0.1', # バージョン
+    license='MIT', # ライセンス
+    install_requires=['numpy', ], # pip installする際に同時にインストールされるパッケージ名をリスト形式で指定
+    author='papillon', # パッケージ作者の名前
+    author_email='ogyogugige@gmail.com', # パッケージ作者の連絡先メールアドレス
+    url='https://github.com/papillon-woof/pyqec', # パッケージに関連するサイトのURL(GitHubなど)
+    description='Simulater for quantum error correction code.', # パッケージの簡単な説明
+    long_description=long_description, # PyPIに'Project description'として表示されるパッケージの説明文
+    long_description_content_type='text/markdown', # long_descriptionの形式を'text/plain', 'text/x-rst', 'text/markdown'のいずれかから指定
+    keywords='pyqec pyQec py-qec qecc qec quantum error correction quantum computer',# PyPIでの検索用キーワードをスペース区切りで指定
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.8.6',
+    ],
+)
