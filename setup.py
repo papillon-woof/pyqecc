2ab0d98805d5dccff26fce429485b0fcc8a05bd9
hy4 <ogyogugige@gmail.com>
2022-02-04 22:45:13 +0900

    delete japanese sentense

--- setup.py
+++ setup.py
@@ -4,12 +4,11 @@ from os import path
 
 here = path.abspath(path.dirname(__file__))
 
-# long_description(後述)に、GitHub用のREADME.mdを指定
 with open(path.join(here, "README.md"), encoding="utf-8") as f:
     long_description = f.read()
 
 setup(
-    name="pyqecc",  # パッケージ名(プロジェクト名)
+    name="pyqecc",  # package name
     packages=[
         "pyqecc",
         "pyqecc.qecc",
@@ -17,18 +16,18 @@ setup(
         "pyqecc.channel",
         "pyqecc.util",
     ],  # パッケージ内(プロジェクト内)のパッケージ名をリスト形式で指定
-    version="0.0.1",  # バージョン
-    license="MIT",  # ライセンス
+    version="0.0.1",  # version
+    license="MIT",  # lisence
     install_requires=[
         "numpy",
-    ],  # pip installする際に同時にインストールされるパッケージ名をリスト形式で指定
-    author="papillon",  # パッケージ作者の名前
-    author_email="ogyogugige@gmail.com",  # パッケージ作者の連絡先メールアドレス
-    url="https://github.com/papillon-woof/pyqec",  # パッケージに関連するサイトのURL(GitHubなど)
-    description="Simulater for quantum error correction code.",  # パッケージの簡単な説明
-    long_description=long_description,  # PyPIに'Project description'として表示されるパッケージの説明文
-    long_description_content_type="text/markdown",  # long_descriptionの形式を'text/plain', 'text/x-rst', 'text/markdown'のいずれかから指定
-    keywords="pyqecc PyQecc py-qecc qecc qec quantum error correction quantum computer",  # PyPIでの検索用キーワードをスペース区切りで指定
+    ],
+    author="papillon",
+    author_email="ogyogugige@gmail.com",
+    url="https://github.com/papillon-woof/pyqecc",
+    description="Simulater for quantum error correction code.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords="pyqecc PyQecc py-qecc qecc qec quantum error correction quantum computer quantum computing",
     classifiers=[
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3.8",
